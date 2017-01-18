5314d76a6474200a6d3542c68c3053bc71ee5d33
Graham Ollis <plicease@cpan.org>
2017-01-18 16:04:12 -0500

    add helper

--- README.md
+++ README.md
@@ -2,6 +2,39 @@
 
 Find or build xz
 
+# SYNOPSIS
+
+From a Perl script
+
+    use Alien::xz;
+    use Env qw( @PATH );
+    unshift @PATH, Alien::xz->bin_dir;  # xz is now in your path
+
+From Alien::Base Build.PL
+
+    use Alien:Base::ModuleBuild;
+    my $builder = Module::Build->new(
+      ...
+      alien_bin_requires => {
+        'Alien::xz' => '0.02',
+      }
+      ...
+    );
+    $builder->create_build_script;
+
+# DESCRIPTION
+
+This package can be used by other CPAN modules that require xz,
+the compression utility.
+
+# HELPERS
+
+## xz
+
+    %{xz}
+
+Returns the name of the xz command.  Usually just `xz`.
+
 # AUTHOR
 
 Graham Ollis <plicease@cpan.org>
--- lib/Alien/xz.pm
+++ lib/Alien/xz.pm
@@ -7,4 +7,46 @@ use base qw( Alien::Base );
 # ABSTRACT: Find or build xz
 # VERSION
 
+=head1 SYNOPSIS
+
+From a Perl script
+
+ use Alien::xz;
+ use Env qw( @PATH );
+ unshift @PATH, Alien::xz->bin_dir;  # xz is now in your path
+
+From Alien::Base Build.PL
+
+ use Alien:Base::ModuleBuild;
+ my $builder = Module::Build->new(
+   ...
+   alien_bin_requires => {
+     'Alien::xz' => '0.02',
+   }
+   ...
+ );
+ $builder->create_build_script;
+
+=head1 DESCRIPTION
+
+This package can be used by other CPAN modules that require xz,
+the compression utility.
+
+=head1 HELPERS
+
+=head2 xz
+
+ %{xz}
+
+Returns the name of the xz command.  Usually just C<xz>.
+
+=cut
+
+sub alien_helper
+{
+  return {
+    xz => sub { 'xz' },
+  };
+}
+
 1;
